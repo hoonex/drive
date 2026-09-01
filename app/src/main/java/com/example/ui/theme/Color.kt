b889e73d186b428a0915db7aecf7001e09732117
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-02 00:58:25 +0900

    style(android): add focused controller color system

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DriveBackground = Color(0xFF070A0E)
+val DriveSurface = Color(0xFF0E1319)
+val DriveSurfaceRaised = Color(0xFF151C24)
+val DriveSurfacePressed = Color(0xFF202B36)
+val DriveBorder = Color(0xFF26313D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DriveAccent = Color(0xFF68C8FF)
+val DriveAccentStrong = Color(0xFF2BAAF2)
+val DriveAccentMuted = Color(0xFF16384C)
+
+val DriveSuccess = Color(0xFF5BE39A)
+val DriveWarning = Color(0xFFFFC95B)
+val DriveDanger = Color(0xFFFF6F7D)
+
+val DriveText = Color(0xFFF4F8FB)
+val DriveTextMuted = Color(0xFF93A1AE)
+val DriveTextFaint = Color(0xFF667481)
